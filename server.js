6d9198fa674db0bc754c59af72229c5d0e2e313e
yas-amini <yaasamin.amini@gmail.com>
2025-05-05 16:46:09 +0200

    set up ejs

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  console.log("Here");
+  res.render("index");
+});
+app.listen(3000);
